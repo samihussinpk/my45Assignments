6493ee36728b7e90199b58f0d0e39b4f8388ac5b
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-07-07 23:27:03 -0700

    Add files via upload

---
+++ _2Task.js
@@ -0,0 +1,5 @@
+"use strict";
+// let personName: string = "sami hussain";
+// console.log(personName.toUpperCase());    //SAMI HUSSAIN
+// console.log(personName.toLowerCase());    //sami hussain
+Object.defineProperty(exports, "__esModule", { value: true });
---
+++ _2Task.ts
@@ -0,0 +1,5 @@
+// let personName: string = "sami hussain";
+// console.log(personName.toUpperCase());    
+// console.log(personName.toLowerCase());    
+
+
