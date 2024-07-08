9a94090fccbf61f83bee750ef6b8e77388521ab8
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-07-07 23:21:04 -0700

    Add files via upload

---
+++ _1Task.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// let personName=" Mr.Sami"
+// console.log(`Welcome ${personName} would you like to learn typeScript ?`);
+//                               Welcome Mr.Sami would you like to learn typeScript ?
---
+++ _1Task.ts
@@ -0,0 +1,3 @@
+// let personName=" Mr.Sami"
+// console.log(`Welcome ${personName} would you like to learn typeScript ?`);
+//                               Welcome Mr.Sami would you like to learn typeScript ?
